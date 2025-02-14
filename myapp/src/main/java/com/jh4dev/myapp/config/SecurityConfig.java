f125dc6cb6532bd0aad50cc218a99567a88b16de
DESKTOP-UJ3854B\LOTTE <jinhyung.ahn@lotte.net>
2025-02-14 09:58:14 +0900

    Security Config
    Security Config

---
+++ myapp/src/main/java/com/jh4dev/myapp/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.jh4dev.myapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
+
+        UserDetails newUser = User.withUsername("user")
+                .password(passwordEncoder().encode("passw0rd"))
+                .authorities("read")
+                .build();
+
+        userDetailsManager.createUser(newUser);
+        return userDetailsManager;
+    }
+
+    @Bean
+    BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    //H2 console 예외처리
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().requestMatchers(new AntPathRequestMatcher("/h2-console/**"));
+    }
+}
